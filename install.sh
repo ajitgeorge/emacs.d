988ee42f58263276c15834942b6f203769e72bf8
Ajit George <ajit@ajitgeorge.com>
2015-03-24 17:51:42 -0400

    install.sh - extract ln fn

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+make_symbolic_link () {
+    local linkfrom=$2
+    local linkto=$1
+
+    if [[ -e $linkfrom ]] ; then
+        if [[ $(readlink $linkfrom) == "$linkto" ]] ; then
+            echo $linkto already installed at $linkfrom
+            exit 0
+        else
+            echo $linkfrom exists but does not point to $linkto.  Aborting.
+            exit 1
+        fi
+    fi
+
+    echo ln -s $linkto $linkfrom
+    ln -s $linkto $linkfrom
+}
+
 here=$(ruby -e 'puts File.absolute_path(".")') # hack around OS X not having have realpath
 
-if [[ -e ~/.emacs.d ]] ; then
-    if [[ $(readlink ~/.emacs.d) == "$here" ]] ; then
-        echo $here already installed
-        exit 0
-    else
-        echo ~/.emacs.d exists but does not point to $here.  Aborting.
-        exit 1
-    fi
-fi
-
-set -x
-ln -s $here ~/.emacs.d
+make_symbolic_link $here ~/.emacs.d
